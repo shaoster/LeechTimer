501c60401d3e598d9481587cde939f00811f1b3f
Phil Shao <shaoster@gmail.com>
2026-02-22 17:20:07 -0500

    History cleanup and tighten state machine

--- src/components/TextBoxContainer.tsx
+++ src/components/TextBoxContainer.tsx
@@ -1,14 +1,15 @@
-import { List, ListItem, Table, TableBody} from "@mui/material";
+import { Button, List, ListItem, Table, TableBody} from "@mui/material";
 import { useEffect, useState } from "react";
 import CopyPastableTextBox from "./CopyPastableTextBox";
 import humanizeDuration from "humanize-duration";
-import { Campaign, NotStarted, PauseCircle, PlayCircle, RestartAlt, CheckCircle } from "@mui/icons-material";
+import { Campaign, NotStarted, PauseCircle, PlayCircle, RestartAlt, CheckCircle, DeleteForever } from "@mui/icons-material";
 import { DataContext, INITIAL_DATA } from "../context";
 
 const TextBoxContainer = () => {
     const [timerRunning, setTimerRunning] = useState(false);
     const [secondsElapsed, setSecondsElapsed] = useState(0);
     const [data, setData] = useState(INITIAL_DATA);
+    const [finished, setFinished] = useState(false)
     const [history, setHistory] = useState<Array<string>>([]);
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -42,19 +43,35 @@ const TextBoxContainer = () => {
         setTimerRunning(true);
         return true;
     };
-    const resumeDisabled = timerRunning || secondsElapsed == 0;
+    const resumeDisabled = timerRunning || secondsElapsed == 0 || finished;
+    const finish = () => {
+        if (!timerRunning) {
+            alert("The timer is not running.");
+            return false;
+        }
+        setTimerRunning(false);
+        setFinished(true);
+        return true;
+    }
     const reset = () => {
         if (confirm("Are you sure you want to reset all the timers?")) {
             setTimerRunning(false);
             setSecondsElapsed(0);
+            setFinished(false);
             return true;
         }
         return false;
     }
+    const resetDisabled = secondsElapsed == 0;
+    const updateDisabled = secondsElapsed == 0 || finished;
     const appendHistory = (text: string) => {
         setHistory((prev) => [...prev, text]);
     };
-
+    const clearHistory = () => {
+        if (confirm("Are you sure you want to clear the event history?")) {
+            setHistory([]);
+        }
+    }
     return (<div>
         <h1>Leech Events</h1>
         <Table>
@@ -84,32 +101,42 @@ const TextBoxContainer = () => {
                     <CopyPastableTextBox
                         label="Finish"
                         icon={<CheckCircle/>}
-                        onCopy={pause}
+                        onCopy={finish}
                         textTemplate={`Done at {time} after {timeElapsed}.`}
                         disabled={pauseDisabled}
+                    />
+                    <CopyPastableTextBox
+                        label="Update"
+                        icon={<Campaign/>}
+                        textTemplate={`{timeElapsed} elapsed.`}
+                        disabled={updateDisabled}
                     />
                     <CopyPastableTextBox
                         label="Reset"
                         icon={<RestartAlt/>}
                         onCopy={reset}
                         textTemplate={`Timers reset.`}
-                    />
-                    <CopyPastableTextBox
-                        label="Update"
-                        icon={<Campaign/>}
-                        textTemplate={`{timeElapsed} elapsed.`}
+                        disabled={resetDisabled}
                     />
                 </DataContext.Provider>
             </TableBody>
         </Table>
-        <h1>History</h1>
-        <List>
-            {history.map((entry)=> (
-                <ListItem>
-                    {entry}
-                </ListItem>
-            ))}
-        </List>
+        <h1>
+            History
+            &nbsp;
+            <Button variant="outlined" startIcon={<DeleteForever/>} onClick={clearHistory} disabled={history.length == 0 || !finished}>
+                Clear
+            </Button>
+        </h1>
+        {history.length == 0 ? <ListItem>No Events Yet</ListItem> :
+            <List>
+                {history.map((entry)=> (
+                    <ListItem>
+                        {entry}
+                    </ListItem>
+                ))}
+            </List>
+        }
     </div>);
 }
 
